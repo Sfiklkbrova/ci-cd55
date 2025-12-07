86ee3e3de7a4c7381794af22e1e538ade6366fc6
Shafiga Alakbarova <sfiqlkbrova65@gmail.com>
2025-12-07 22:42:57 +0400

    dfghj

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    port:5161,
+    allowedHosts:["ci-cd55.alakbarova.site"]
+  },
+  preview:{
+    port:5161,
+    host:true
+  },
   plugins: [react()],
 })
